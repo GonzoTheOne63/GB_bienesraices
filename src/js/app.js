b5727703a07a46bc9daa5108e4317fba6fa183dc
GoinBar <gbarron.ms@gmail.com>
2023-01-07 08:04:34 -0600

    CerrandoProyectoEstático
    Signed-off-by: GoinBar <gbarron.ms@gmail.com>

--- src/js/app.js
+++ src/js/app.js
@@ -1,44 +1,49 @@
-document.addEventListener('DOMContentLoaded', function() {
+/* ESPERA para que el documento este completamente cargado  */
+document.addEventListener("DOMContentLoaded", function () {
+  eventListeners();
 
-    eventListeners();
-
-    darkMode();
+  darkMode();
 });
 
 function darkMode() {
+  const prefiereDarkMode = window.matchMedia("(prefers-color-scheme: dark)");
 
-    const prefiereDarkMode = window.matchMedia('(prefers-color-scheme: dark)');
+  // console.log(prefiereDarkMode.matches);
 
-    // console.log(prefiereDarkMode.matches);
+  if (prefiereDarkMode.matches) {
+    document.body.classList.add("dark-mode");
+  } else {
+    document.body.classList.remove("dark-mode");
+  }
 
-    if(prefiereDarkMode.matches) {
-        document.body.classList.add('dark-mode');
+  prefiereDarkMode.addEventListener("change", function () {
+    if (prefiereDarkMode.matches) {
+      document.body.classList.add("dark-mode");
     } else {
-        document.body.classList.remove('dark-mode');
+      document.body.classList.remove("dark-mode");
     }
+  });
 
-    prefiereDarkMode.addEventListener('change', function() {
-        if(prefiereDarkMode.matches) {
-            document.body.classList.add('dark-mode');
-        } else {
-            document.body.classList.remove('dark-mode');
-        }
-    });
-
-    const botonDarkMode = document.querySelector('.dark-mode-boton');
-    botonDarkMode.addEventListener('click', function() {
-        document.body.classList.toggle('dark-mode');
-    });
+  const botonDarkMode = document.querySelector(".dark-mode-boton");
+  botonDarkMode.addEventListener("click", function () {
+    document.body.classList.toggle("dark-mode");
+  });
 }
 
 function eventListeners() {
-    const mobileMenu = document.querySelector('.mobile-menu');
+  const mobileMenu = document.querySelector(".mobile-menu");
 
-    mobileMenu.addEventListener('click', navegacionResponsive);
+  mobileMenu.addEventListener("click", navegacionResponsive);
 }
 
 function navegacionResponsive() {
-    const navegacion = document.querySelector('.navegacion');
+  const navegacion = document.querySelector(".navegacion");
 
-    navegacion.classList.toggle('mostrar')
+  if (navegacion.classList.contains("mostrar")) {
+    navegacion.classList.remove("mostrar");
+  } else {
+    navegacion.classList.add("mostrar");
+  }
+
+//   navegacion.classList.toggle("mostrar"); // MODO de codigo avanzado
 }
